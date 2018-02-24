80a3cbc014be185d561a7519891ab6fb17ac1fee
lixiaonan01 <lixiaonan01@baidu.com>
2018-02-24 22:29:18 +0800

    添加媒体播放计时器类

---
+++ src/media/base/playback_clock.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+#include "playback_clock.h"
+
+namespace media {
+PlaybackClock::PlaybackClock()
+    : is_ticking_(false), playback_rate_(1.0), current_media_time_(0) {
+}
+
+void PlaybackClock::StartTicking() {
+  wall_clock_timer_.Start();
+  is_ticking_ = true;
+}
+
+void PlaybackClock::StopTicking() {
+  wall_clock_timer_.Stop();
+  is_ticking_ = false;
+}
+
+void PlaybackClock::SetPlaybackRate(float rate) {
+  current_media_time_ += static_cast<int64_t>(wall_clock_timer_.Elapsed() * playback_rate_);
+  playback_rate_ = rate;
+  wall_clock_timer_.Restart();
+}
+
+void PlaybackClock::SetStartTime(int64_t start_time) {
+  if (is_ticking_)
+    return;
+  current_media_time_ = start_time;
+}
+
+int64_t PlaybackClock::GetCurrentMediaTime() {
+  current_media_time_ = static_cast<int64_t>(wall_clock_timer_.Elapsed() * playback_rate_);
+  return current_media_time_;
+}
+
+}  // namespace media
